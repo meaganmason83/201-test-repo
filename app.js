43b9f2aec9cc1ce765b04a1b4568e080d9087ac9
Meagan Mason <meaganmason83@gmail.com>
2016-09-15 18:22:30 -0700

    updates

--- app.js
+++ app.js
@@ -4,53 +4,57 @@ alert('Welcome to my site, ' + userName + '!');
 var questionOne = prompt("Was I born in Florida?").toLowerCase();
 
 if (questionOne === 'yes' || questionOne === 'y'){
-alert("Yup, you got it!");
-} else {
+  alert("Yup, you got it!");
+} else if (questionOne === 'no' || questionOne === 'n'){
   alert("Incorrect.");
+} else {
+  alert("You must answer yes or no.");
 }
 
 var questionTwo = prompt("Do you think I'm really geeky?").toLowerCase();
 
 if (questionTwo === 'yes' || questionTwo === 'y'){
   alert("Nah, I'm not super geeky.");
-} else {
+} else if (questionTwo === 'no' || questionTwo === 'n'){
   alert("You're right, I'm only half-geek.");
+} else {
+  alert("You must answer yes or no.");
 }
 
 var questionThree = prompt("Do you think I'm over 30?").toLowerCase();
 
 if (questionThree === 'yes' || questionThree === 'y') {
   alert("Don't worry, you aren't in trouble. I am over 30! Plus, 30 is the new 20.");
-} else {
+} else if (questionThree === 'no' || questionThree === 'n'){
   alert("You are too kind.");
+} else {
+  alert("You must answer yes or no.");
 }
 
 var questionFour = prompt("Are you sick of all the questions?").toLowerCase();
 
 if (questionFour === 'yes' || questionFour === 'y'){
   alert("Ok, cool. I am too. Goodnight!");
-} else {
+} else if (questionFour === 'no' || questionFour === 'n'){
   alert("Alright let me think of some more then.");
-};
-
-var questionFive = prompt(" ").toLowerCase();
-
-if (){
-  /* One of these questions should include the use of logical operators
-  (refer to page ~157 of the JS text book for example usage).
-  As a secondary example, one of your questions can validate use input in the form of 'Yes' and 'Y', or 'No' and 'N'.
 } else {
+  alert("You must answer yes or no.");
+}
 
-}*/
+var qestionFive = prompt("Would you say I'm at least 5 feet tall?").toLowerCase();
+
+if (questionFive === 'yes' || questionFive === 'y'){
+  alert("Good, you don't think I'm a midget.");
+} else if (questionFive === 'no' || questionFive === 'n'){
+  alert("You really think I'm a midget?");
+} else {
+  alert("You must answer yes or no.");
+}
 
 var questionSix = prompt(" ").toLowerCase();
 
-if() {
-  /* One of the questions should include multiple answers (more than two).
-  These answers can be stored in an array, and you will want to use the array method indexOf()
-  to find out whether the user's guess exists in the array or not.
-  If it does, use the index returned to you to select that value
-  and tell the user that they guessed the answer correct.
-} else {
+var myArray = ['cat', 'bunny', 'dog'];
 
-}*/
+if (questionSix === myArray.indexOf) {
+  alert("You know me so well!")
+} else if (questionSix )
