23b36db4afc1ce3199938fb10055d6d1dc940bd5
Meagan Mason <meaganmason83@gmail.com>
2016-09-15 21:24:22 -0700

    Assignment lab for class 2

--- app.js
+++ app.js
@@ -1,60 +1,61 @@
 var userName = prompt('What\'s your name?');
 alert('Welcome to my site, ' + userName + '!');
 
-var questionOne = prompt("Was I born in Florida?").toLowerCase();
+var questionOne = prompt("Do you think I'm from the United States?").toLowerCase();
 
 if (questionOne === 'yes' || questionOne === 'y'){
   alert("Yup, you got it!");
 } else if (questionOne === 'no' || questionOne === 'n'){
-  alert("Incorrect.");
+  alert("You think I'm exotic then, how sweet!");
 } else {
-  alert("You must answer yes or no.");
+  alert("Please answer yes or no.");
 }
 
-var questionTwo = prompt("Do you think I'm really geeky?").toLowerCase();
+var questionTwo = prompt("Do you think I'm a big geek?").toLowerCase();
 
 if (questionTwo === 'yes' || questionTwo === 'y'){
-  alert("Nah, I'm not super geeky.");
+  alert("How dare you.");
 } else if (questionTwo === 'no' || questionTwo === 'n'){
   alert("You're right, I'm only half-geek.");
 } else {
-  alert("You must answer yes or no.");
+  alert("Please answer yes or no.");
 }
 
-var questionThree = prompt("Do you think I'm over 30?").toLowerCase();
+var questionThree = prompt("Do you think I still get carded?").toLowerCase();
 
 if (questionThree === 'yes' || questionThree === 'y') {
-  alert("Don't worry, you aren't in trouble. I am over 30! Plus, 30 is the new 20.");
+  alert("You're right, and I take it as a compliment. Besides, 33 is the new 23!");
 } else if (questionThree === 'no' || questionThree === 'n'){
-  alert("You are too kind.");
+  alert("How dare you.");
 } else {
-  alert("You must answer yes or no.");
+  alert("Please answer yes or no.");
 }
 
-var questionFour = prompt("Are you sick of all the questions?").toLowerCase();
+var questionFour = prompt("Do you think I have road rage?").toLowerCase();
 
 if (questionFour === 'yes' || questionFour === 'y'){
-  alert("Ok, cool. I am too. Goodnight!");
+  alert("Guitly as charged.");
 } else if (questionFour === 'no' || questionFour === 'n'){
-  alert("Alright let me think of some more then.");
+  alert("Ohhh, you have no idea! LOL");
 } else {
-  alert("You must answer yes or no.");
+  alert("Please answer yes or no.");
 }
 
-var qestionFive = prompt("Would you say I'm at least 5 feet tall?").toLowerCase();
+var questionFive = prompt("Would you say I'm at least 5 feet tall?").toLowerCase();
 
 if (questionFive === 'yes' || questionFive === 'y'){
   alert("Good, you don't think I'm a midget.");
 } else if (questionFive === 'no' || questionFive === 'n'){
   alert("You really think I'm a midget?");
 } else {
-  alert("You must answer yes or no.");
+  alert("Please answer yes or no.");
 }
 
-var questionSix = prompt(" ").toLowerCase();
+var questionSix = prompt("I'm thinking of a number between 1 and 5. What is it?");
 
-var myArray = ['cat', 'bunny', 'dog'];
-
-if (questionSix === myArray.indexOf) {
-  alert("You know me so well!")
-} else if (questionSix )
+var myArray = [3, 4];
+if (myArray.indexOf(questionSix) > -1) {
+  alert("You can read my mind!");
+} else {
+  alert("You aren't a mind reader afterall.");
+}
